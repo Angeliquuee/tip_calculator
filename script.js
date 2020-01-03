9d1a8b4a1ca989d2ecddf2af3f9b508e158d9e33
Angeliquuee <angelique.ocular@gmail.com>
2020-01-02 17:35:18 -0800

    Added tweaks and adjust output

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const button = document.querySelector("button");
 const output = document.querySelector(".output");
+const cost = document.querySelector("input");
 console.log(button);
 button.addEventListener("click", function(){
-    console.log("click");
-    output.innerText = "output";
+    const cost = document.querySelector("input");
+    console.log(cost.value*0.15);
+    let tip = (cost.value*0.15).toFixed(2);
+    let temp = '<h1> You should tip $'+tip+' on $'+cost.value+'.</h1>';
+    output.innerHTML = temp;
 })
